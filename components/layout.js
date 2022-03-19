a8866e69a4f4431bd6e0e5569e103fbbada3f832
luisbebop <luisbebop@gmail.com>
2022-03-19 03:01:33 -0300

    Change og card

--- components/layout.js
+++ components/layout.js
@@ -14,13 +14,13 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Faça doações via PIX para a Ucrânia."
         />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fimages-cdn.9gag.com%2Fphoto%2FaWEW8KZ_700b.jpg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
